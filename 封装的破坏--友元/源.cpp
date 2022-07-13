27d95609413e25a5ea12c5d9d97f7fae32367590
Alvin <82175679+Alvin-ljh@users.noreply.github.com>
2022-07-13 18:18:46 +0800

    Update 源.cpp

--- 封装的破坏--友元/源.cpp
+++ 封装的破坏--友元/源.cpp
@@ -1,45 +1,45 @@
-//// µ—È∆ﬂµ⁄“ªÃ‚
-//#include<iostream>
-//#include<cmath>
-//using namespace std;
-//class Point
-//{
-//public:
-//	Point(float = 0, float = 0);
-//	void set();
-//	void show();
-//	friend void dis(Point, Point);
-//private:
-//	float x, y;
-//};
-//
-//Point::Point(float x, float y)
-//{
-//	this->x = x; this->y = y;
-//}
-//
-//void Point::set()
-//{
-//	cout << "Please enter the coordinate of a point:";
-//	cin >> x >> y;
-//}
-//void Point::show() { cout << "(x,y)=" << "(" << x << "," << y << ")" << endl; }
-//void dis(Point p1, Point p2)
-//{
-//	float d = sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-//	cout << "The distance id:" << d << endl;
-//}
-//
-//int main()
-//{
-//	Point p1(1, 2), p2(3, 4);
-//	p1.show();
-//	p2.show();
-//	dis(p1, p2);
-//	return 0;
-//}
+//ÂÆûÈ™å‰∏ÉÁ¨¨‰∏ÄÈ¢ò
+#include<iostream>
+#include<cmath>
+using namespace std;
+class Point
+{
+public:
+	Point(float = 0, float = 0);
+	void set();
+	void show();
+	friend void dis(Point, Point);
+private:
+	float x, y;
+};
 
-// µ—È∆ﬂµ⁄∂˛Ã‚
+Point::Point(float x, float y)
+{
+	this->x = x; this->y = y;
+}
+
+void Point::set()
+{
+	cout << "Please enter the coordinate of a point:";
+	cin >> x >> y;
+}
+void Point::show() { cout << "(x,y)=" << "(" << x << "," << y << ")" << endl; }
+void dis(Point p1, Point p2)
+{
+	float d = sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	cout << "The distance id:" << d << endl;
+}
+
+int main()
+{
+	Point p1(1, 2), p2(3, 4);
+	p1.show();
+	p2.show();
+	dis(p1, p2);
+	return 0;
+}
+
+//ÂÆûÈ™å‰∏ÉÁ¨¨‰∫åÈ¢ò
 #include<iostream>
 using namespace std;
 class Score
@@ -62,108 +62,107 @@ int main()
 {
 	Score* p = NULL;
 	int s, count = 1;
-	cout << " ‰»Î“ª∏ˆ∞‡µƒ—ß…˙≥…º®÷±µΩ ‰»Î-1Œ™÷π£°" << endl;
+	cout << "ËæìÂÖ•‰∏Ä‰∏™Áè≠ÁöÑÂ≠¶ÁîüÊàêÁª©Áõ¥Âà∞ËæìÂÖ•-1‰∏∫Ê≠¢ÔºÅ" << endl;
 	while (1)
 	{
-		cout << "«Î ‰»Îµ⁄" << count << "∏ˆ—ß…˙µƒ∑÷ ˝" << ":";
+		cout << "ËØ∑ËæìÂÖ•Á¨¨" << count << "‰∏™Â≠¶ÁîüÁöÑÂàÜÊï∞" << ":";
 		cin >> s;
 		if (s == -1) { delete p; break; }
 		p = new Score(s);
 		count++;
 	}
 	count--;
-	cout << "◊‹∑÷" << Score::getSum() << endl;
-	cout << "∆Ωæ˘∑÷" << Score::getSum() / count << endl;
+	cout << "ÊÄªÂàÜ" << Score::getSum() << endl;
+	cout << "Âπ≥ÂùáÂàÜ" << Score::getSum() / count << endl;
 
 	return 0;
 }
 
-//#include<iostream>
-//using namespace std;
-//class Date
-//{
-//public:
-//	Date() {
-//		month = 1;
-//		day = 1;
-//		year = 2020;
-//		cout << "Œﬁ≤Œππ‘Ï∫Ø ˝‘À––≥…π¶" << endl;
-//	}
-//	Date(int M, int D, int Y) {
-//		month = M;
-//		day = D;
-//		year = Y;
-//		cout << "”–≤Œππ‘Ï∫Ø ˝‘À––≥…π¶" << endl;
-//	}
-//	void setDate(int M, int D, int Y) {
-//		month = M;
-//		day = D;
-//		year = Y;
-//	}
-//	friend void Print(Date&);
-//private:
-//	int month;
-//	int day;
-//	int year;
-//};
-//void Print(Date& dd) {
-//	cout << "»’∆⁄£∫" << dd.year << "ƒÍ" << dd.month << "‘¬" << dd.day << "»’" << endl;
-//}
-//int main()
-//{
-//	Date d1, d2(4, 14, 2022);
-//	Print(d1); Print(d2);
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date() {
+		month = 1;
+		day = 1;
+		year = 2020;
+		cout << "Êó†ÂèÇÊûÑÈÄ†ÂáΩÊï∞ËøêË°åÊàêÂäü" << endl;
+	}
+	Date(int M, int D, int Y) {
+		month = M;
+		day = D;
+		year = Y;
+		cout << "ÊúâÂèÇÊûÑÈÄ†ÂáΩÊï∞ËøêË°åÊàêÂäü" << endl;
+	}
+	void setDate(int M, int D, int Y) {
+		month = M;
+		day = D;
+		year = Y;
+	}
+	friend void Print(Date&);
+private:
+	int month;
+	int day;
+	int year;
+};
+void Print(Date& dd) {
+	cout << "Êó•ÊúüÔºö" << dd.year << "Âπ¥" << dd.month << "Êúà" << dd.day << "Êó•" << endl;
+}
+int main()
+{
+	Date d1, d2(4, 14, 2022);
+	Print(d1); Print(d2);
+	return 0;
 
-//#include<iostream>
-//using namespace std;
-//class Book
-//{
-//public:
-//	Book(string N, int P) {
-//		name = N;
-//		price = P;
-//		num++;
-//		totalPrice += P;
-//		cout << "ππ‘Ï∫Ø ˝‘À––≥…π¶" << endl;
-//	}
-//	Book(int P) {
-//		price = P;
-//		num++;
-//		totalPrice += P;
-//		cout << "ππ‘Ï∫Ø ˝‘À––≥…π¶" << endl;
-//	}
-//	static int num;
-//	static int totalPrice;
-//	void showInfo() {
-//		//∏¯∂ØÃ¨¥¥Ω®µƒ∂‘œÛ–¥“ª∏ˆshow∫Ø ˝
-// 	}
-//private:
-//	string name;
-//	int price;
-//};
-//int Book::num = 0;
-//int Book::totalPrice = 0;
-//int main()
-//{
-//	Book* p = nullptr;
-//	int price, count = 1;
-//	string name;
-//	cout << " ‰»Î“ª≈˙ Èµƒ È√˚∫Õº€∏Ò÷±µΩ ‰»Î-1Œ™÷π" << endl;
-//	while (1) {
-//		cout << "«Î ‰»Îµ⁄" << count << "±æ Èµƒ È√˚∫Õº€∏Ò" << "£∫";
-//		cin >> name >> price;
-//		if (name  == "-1" && price == -1) {
-//			delete p;
-//			break;
-//		}
-//		p = new Book(name, price);
-//		cout << p << endl;
-//		count++;
-//	}
-//	count--;
-//	cout << "Õº È◊‹≤· ˝£∫" << Book::num << endl;
-//	cout << "Õº È◊‹º€£∫" << Book::totalPrice << endl;
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+class Book
+{
+public:
+	Book(string N, int P) {
+		name = N;
+		price = P;
+		num++;
+		totalPrice += P;
+		cout << "ÊûÑÈÄ†ÂáΩÊï∞ËøêË°åÊàêÂäü" << endl;
+	}
+	Book(int P) {
+		price = P;
+		num++;
+		totalPrice += P;
+		cout << "ÊûÑÈÄ†ÂáΩÊï∞ËøêË°åÊàêÂäü" << endl;
+	}
+	static int num;
+	static int totalPrice;
+	void showInfo() {
+		//ÁªôÂä®ÊÄÅÂàõÂª∫ÁöÑÂØπË±°ÂÜô‰∏Ä‰∏™showÂáΩÊï∞
+ 	}
+private:
+	string name;
+	int price;
+};
+int Book::num = 0;
+int Book::totalPrice = 0;
+int main()
+{
+	Book* p = nullptr;
+	int price, count = 1;
+	string name;
+	cout << "ËæìÂÖ•‰∏ÄÊâπ‰π¶ÁöÑ‰π¶ÂêçÂíå‰ª∑Ê†ºÁõ¥Âà∞ËæìÂÖ•-1‰∏∫Ê≠¢" << endl;
+	while (1) {
+		cout << "ËØ∑ËæìÂÖ•Á¨¨" << count << "Êú¨‰π¶ÁöÑ‰π¶ÂêçÂíå‰ª∑Ê†º" << "Ôºö";
+		cin >> name >> price;
+		if (name  == "-1" && price == -1) {
+			delete p;
+			break;
+		}
+		p = new Book(name, price);
+		cout << p << endl;
+		count++;
+	}
+	count--;
+	cout << "Âõæ‰π¶ÊÄªÂÜåÊï∞Ôºö" << Book::num << endl;
+	cout << "Âõæ‰π¶ÊÄª‰ª∑Ôºö" << Book::totalPrice << endl;
+	return 0;
+}
